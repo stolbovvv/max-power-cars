3b1a1e28d8dd98bc90bd4b426d3b32cd0e0fcf2f
Valerii Stolbov <stolbov.v@outlook.com>
2022-12-14 17:22:57 +0500

    create profile page

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,3 +1,32 @@
+/* global tns */
 window.addEventListener('DOMContentLoaded', () => {
-  // User scripts
+  // Sliders
+  const sliderCelectedCar = document.querySelector('.slider-selected-car');
+
+  function creatSliderDots(count, container) {
+    for (let i = 0; i < count; i++) {
+      container.insertAdjacentHTML('beforeEnd', '<span class="slider-selected-car__dots-item"></span>');
+    }
+  }
+
+  if (sliderCelectedCar) {
+    const slideCount = sliderCelectedCar.querySelectorAll('[data-slider="slide"]').length;
+    const slideContainer = sliderCelectedCar.querySelector('[data-slider="slide-container"]');
+    const dotsContainer = sliderCelectedCar.querySelector('[data-slider="dot-container"]');
+    const butttonPrev = sliderCelectedCar.querySelector('[data-slider="button-prev"]');
+    const butttonNext = sliderCelectedCar.querySelector('[data-slider="button-next"]');
+
+    if (slideCount) creatSliderDots(slideCount, dotsContainer);
+
+    tns({
+      container: slideContainer,
+      navContainer: dotsContainer,
+      prevButton: butttonPrev,
+      nextButton: butttonNext,
+      items: 1,
+      mode: 'gallery',
+    });
+  } else {
+    console.warn('Warning: No slider with class "slider-selected-car" found on page');
+  }
 });
