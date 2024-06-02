6f645a9dc46f34c14590fffd1205de60501beb50
Valerii Stolbov <stolbov.v@outlook.com>
2024-06-02 21:28:22 +0500

    Created page media

--- src/assets/scripts/bundle.js
+++ src/assets/scripts/bundle.js
@@ -1,3 +1,51 @@
+/* global Splide */
+
 window.addEventListener('DOMContentLoaded', () => {
-	// Custom scripts
+	// Sliders
+	const mediaSlider = document.querySelector('#media-slider');
+	const photoSlider = document.querySelector('#photo-slider');
+
+	if (mediaSlider) {
+		new Splide(mediaSlider, {
+			pagination: false,
+			grid: {
+				rows: 2,
+				cols: 4,
+				gap: {
+					row: '20px',
+					col: '20px',
+				},
+			},
+			breakpoints: {
+				1100: {
+					grid: {
+						cols: 3,
+					},
+				},
+				767: {
+					grid: {
+						rows: 1,
+						cols: 2,
+						gap: {
+							row: '10px',
+							col: '10px',
+						},
+					},
+				},
+				575: {
+					grid: {
+						cols: 1,
+					},
+				},
+			},
+		}).mount(window.splide.Extensions);
+	}
+
+	if (photoSlider) {
+		new Splide(photoSlider, {
+			type: 'slide',
+			gap: '20px',
+			pagination: false,
+		}).mount(window.splide.Extensions);
+	}
 });
