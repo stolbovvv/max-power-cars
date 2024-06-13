81e0eed09600332837fd07cabe72c3aa8ba7a637
Valerii Stolbov <stolbov.v@outlook.com>
2024-06-13 07:55:52 +0500

    Work on modal

--- src/assets/scripts/bundle.js
+++ src/assets/scripts/bundle.js
@@ -47,6 +47,91 @@ class Menu {
 	}
 }
 
+class Tabs {
+	constructor(element = '.js-tabs', { activeClass = 'is-current' } = {}) {
+		this.tabsRoot = typeof element === 'string' ? document.querySelector(element) : element;
+
+		if (this.tabsRoot) {
+			this.tabsTrigger = this.tabsRoot.querySelectorAll('.js-tabs-trigger');
+			this.tabsContent = this.tabsRoot.querySelectorAll('.js-tabs-content');
+
+			this.options = {
+				activeClass,
+			};
+
+			this.update = this.update.bind(this);
+
+			this.init();
+		}
+	}
+
+	update(tabId) {
+		this.tabsTrigger.forEach((trigger) => {
+			if (trigger.dataset.tabId === tabId) trigger.classList.add(this.options.activeClass);
+			if (trigger.dataset.tabId !== tabId) trigger.classList.remove(this.options.activeClass);
+		});
+
+		this.tabsContent.forEach((content) => {
+			if (content.dataset.tabId === tabId) content.classList.add(this.options.activeClass);
+			if (content.dataset.tabId !== tabId) content.classList.remove(this.options.activeClass);
+		});
+	}
+
+	init() {
+		this.tabsTrigger.forEach((trigger) => {
+			trigger.addEventListener('click', () => this.update(trigger.dataset.tabId));
+		});
+
+		this.update(this.tabsTrigger[0].dataset.tabId);
+	}
+}
+
+class Popup {
+	constructor(element = '.js-popup', { activeClass = 'is-active', needLock = true } = {}) {
+		this.popupRoot = typeof element === 'string' ? document.querySelector(element) : element;
+
+		if (this.popupRoot) {
+			this.popupTrigger = document.querySelectorAll(`.js-popup-trigger`);
+
+			this.options = {
+				needLock,
+				activeClass,
+			};
+
+			this.init();
+		}
+	}
+
+	show() {
+		this.popupRoot.classList.add(this.options.activeClass);
+
+		if (this.options.needLock) document.body.style['overflow'] = 'hidden';
+		if (this.options.needLock) document.body.style['scrollbar-gutter'] = 'stable';
+		if (this.options.needLock) document.documentElement.style['scrollbar-gutter'] = 'stable';
+	}
+
+	hide() {
+		this.popupRoot.classList.remove(this.options.activeClass);
+
+		if (this.options.needLock) document.body.style.removeProperty('overflow');
+		if (this.options.needLock) document.body.style.removeProperty('scrollbar-gutter');
+		if (this.options.needLock) document.documentElement.style.removeProperty('scrollbar-gutter');
+	}
+
+	init() {
+		this.popupRoot.addEventListener('click', (e) => {
+			if (e.target && e.target === this.popupRoot) this.hide();
+		});
+
+		this.popupTrigger.forEach((trigger) => {
+			trigger.addEventListener('click', () => {
+				if (trigger.dataset.showPopup === this.popupRoot.id) this.show();
+				if (trigger.dataset.hidePopup === this.popupRoot.id) this.hide();
+			});
+		});
+	}
+}
+
 class Sticky {
 	constructor(selector = '.js-sticky', { activeClass = 'is-active' } = {}) {
 		this.element = document.querySelector(selector);
@@ -78,6 +163,12 @@ window.addEventListener('DOMContentLoaded', () => {
 	// Site menu
 	new Menu();
 
+	// Site tabs
+	new Tabs();
+
+	// Site Popup
+	new Popup();
+
 	// Sticky header
 	new Sticky();
 
@@ -85,6 +176,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	const mediaSlider = document.querySelector('#media-slider');
 	const photoSlider = document.querySelector('#photo-slider');
 	const compareSlider = document.querySelector('#compare-slider');
+	const popupMainSlider = document.querySelector('#popup-slider');
 
 	if (mediaSlider) {
 		new Splide(mediaSlider, {
@@ -141,6 +233,13 @@ window.addEventListener('DOMContentLoaded', () => {
 					perPage: 1,
 				},
 			},
+		}).mount();
+	}
+
+	if (popupMainSlider) {
+		new Splide(popupMainSlider, {
+			arrows: false,
+			pagination: true,
 		}).mount();
 	}
 
